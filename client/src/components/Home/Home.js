976812fb981b523b1095d9aed9f4b63c8ebce604
May Thinzar Oo <may.wytu@gmail.com>
2022-03-23 15:08:26 +0630

    add Navbar for homepage

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from "../Nav/NavBar";
+import './Home.css';
+
+function Home() {
+  return (
+    <NavBar bg="light" />
+  );
+}
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import Home from './components/Home/Home';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
