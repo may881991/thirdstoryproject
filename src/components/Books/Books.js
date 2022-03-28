451a07e01380c0546f8868ccac99dfdb5a27a160
May Thinzar Oo <may.wytu@gmail.com>
2022-03-28 10:33:54 +0630

    add icons from react icon

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Books.css';
 import { Container, Row, Col ,Form ,Button } from "react-bootstrap";
+import { BsSearch } from "react-icons/bs";
 import Carousel from 'react-bootstrap/Carousel';
 import treeImg from "../../assets/images/tree.png";
 import puloneImg from "../../assets/images/pulone.png";
@@ -27,6 +28,7 @@ function Books() {
                 <p className="p-3">Our books are written by Myanmar authors and illustrated by Myanmar illustrators for a Myanmar audience.  They are first and foremost entertaining and fun to read, but they also have important messages addressing peace, tolerance, diversity, girl empowerment, environment, disability rights and child rights. </p>
                 <Form id='search' className='p-1 col-md-10 mx-auto'>
                       <Form.Control type="email" placeholder="Explore More Books" className='text-center'/>
+                      <BsSearch />
                 </Form>
                 </Col>
                 <Col md={2} className="py-3">
